c3c7c59abda952a15ede5de1ab10162229b35719
Aizul <maizulfikrifadilah@gmail.com>
2024-12-11 15:40:26 +0700

    menambahkan file baru

--- syahriah1.sql
+++ syahriah1.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 27 Nov 2024 pada 07.02
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.0.30
+-- Generation Time: Dec 11, 2024 at 08:47 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `laporan`
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'maizulfikrifadilah');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `laporan`
 --
 
 CREATE TABLE `laporan` (
@@ -37,28 +56,48 @@ CREATE TABLE `laporan` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pembayaran`
+-- Table structure for table `pembayaran`
 --
 
 CREATE TABLE `pembayaran` (
-  `nama` varchar(50) DEFAULT NULL,
-  `nis` varchar(10) DEFAULT NULL,
-  `bulan` int(11) DEFAULT NULL,
-  `status` varchar(20) DEFAULT NULL,
-  `jumlah` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `nis` varchar(20) NOT NULL,
+  `bulan` varchar(20) NOT NULL,
+  `tanggal_bayar` date DEFAULT NULL,
+  `jumlah` decimal(10,2) NOT NULL DEFAULT 0.00,
+  `status` enum('Lunas','Belum Bayar') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `pembayaran`
+-- Dumping data for table `pembayaran`
 --
 
-INSERT INTO `pembayaran` (`nama`, `nis`, `bulan`, `status`, `jumlah`) VALUES
-(NULL, '201', 0, 'Lunas', 20000);
+INSERT INTO `pembayaran` (`id`, `nama`, `nis`, `bulan`, `tanggal_bayar`, `jumlah`, `status`) VALUES
+(7, '', '2001', 'januari', NULL, 20000.00, 'Lunas'),
+(8, '', '2002', 'januari', NULL, 30000.00, ''),
+(9, '', '21', 'januari', NULL, 20000.00, 'Lunas'),
+(10, '', '2001', 'januari', NULL, 20000.00, 'Lunas'),
+(11, '', '201', 'januari', NULL, 30000.00, 'Lunas'),
+(12, '', '2000', 'januari', NULL, 10000.00, ''),
+(13, '', '2001', 'januari', NULL, 20000.00, 'Lunas'),
+(15, '', '201', 'januari', NULL, 30000.00, 'Lunas'),
+(16, '', '201', 'januari', NULL, 30000.00, ''),
+(17, '', '201', 'januari', NULL, 20000.00, 'Lunas'),
+(19, '', '888', 'agustus', NULL, 20000.00, 'Lunas'),
+(20, '', '22222', 'agustus', NULL, 30000.00, 'Lunas'),
+(21, '', '2012', 'desember', NULL, 30000.00, ''),
+(22, '', '2003', 'maret', NULL, 30000.00, ''),
+(23, '', '201', 'maret', NULL, 30000.00, 'Lunas'),
+(24, '', '2001', 'september', NULL, 30000.00, 'Lunas'),
+(25, '', '201', 'januari', NULL, 30000.00, 'Lunas'),
+(26, '', '202257001', 'januari', NULL, 20000.00, 'Lunas'),
+(27, '', '202257002', 'agustus', NULL, 20000.00, 'Lunas');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `santri`
+-- Table structure for table `santri`
 --
 
 CREATE TABLE `santri` (
@@ -69,13 +108,44 @@ CREATE TABLE `santri` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `santri`
+-- Dumping data for table `santri`
 --
 
 INSERT INTO `santri` (`nis`, `nama`, `kelas`, `alamat`) VALUES
-(201, 'ijul', 1, 'klampis'),
-(123, 'seh', 1, 'sitanggal'),
-(1234, 'oi', 1, 'tanggerang');
+(202257001, 'ijul', 3, 'kemplang'),
+(202257002, 'ari', 3, 'jatirokeh');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
